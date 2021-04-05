b6a0fa49c57c12b6ace1c42cf12d97f1a47244ba
artisan1218 <zcy1218@gmail.com>
2021-04-05 00:55:02 -0700

    update

--- 1. twoSum/twoSum.java
+++ 1. twoSum/twoSum.java
@@ -5,14 +5,33 @@ public class twoSum {
 
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	int[] test = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	int[] result = twoSumSol(test, 5);
+	int[] test = new int[] { 2, 7, 11, 15 };
+	int[] result = hashMapOnePass(test, 9);
 	for (int i = 0; i < result.length; i++) {
 	    System.out.print(result[i] + " ");
 	}
     }
 
-    public static int[] twoSumSol(int[] nums, int target) {
+    public static int[] hashMapOnePass(int[] nums, int target) {
+	HashMap<Integer, Integer> map = new HashMap<>();
+	int[] result = new int[2];
+
+	for (int i = 0; i < nums.length; i++) {
+	    int addend = nums[i];
+	    int complement = target - addend;
+	    if (map.containsKey(complement)) {
+		result[0] = map.get(complement);
+		result[1] = i;
+		return result;
+	    } else {
+		map.put(addend, i);
+	    }
+	}
+
+	return result;
+    }
+
+    public static int[] bruteForce(int[] nums, int target) {
 	int[] result = new int[2];
 	int addend = 0;
 	int addendIt = 0;
@@ -31,7 +50,7 @@ public class twoSum {
 	return result;
     }
 
-    public static int[] twoSumQuick(int[] nums, int target) {
+    public static int[] hashMapTwoPass(int[] nums, int target) {
 
 	Map<Integer, Integer> s = new HashMap<>();
 	for (int i = 0; i < nums.length; i++) {
