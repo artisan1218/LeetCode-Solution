0e200be24f9999389f102a716b505479bcbd2f33
artisan1218 <zcy1218@gmail.com>
2021-05-12 23:09:44 -0700

    Update longestPalindrome.java

--- 5. lonegstPalindromicSubstring/longestPalindrome.java
+++ 5. lonegstPalindromicSubstring/longestPalindrome.java
@@ -3,7 +3,44 @@ public class longestPalindrome {
 
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	System.out.println(longestPalindromeMethodDP("bccbcd"));
+	System.out.println(longestPalindromeMethodCleanDP("bccbcd"));
+    }
+
+    public static String longestPalindromeMethodCleanDP(String s) {
+	if (s.length() == 0) {
+	    return "";
+	} else {
+	    int start = 0;
+	    int end = 0;
+	    // go through each character in the string s
+	    for (int i = 0; i < s.length(); i++) {
+		// the palindrome has odd length, expand around single point
+		int oddLen = expand(s, i, i);
+		// the palindrome has even length, expand around two points, here we assume that
+		// i and i+1 are same character, because this won't affect the result and is
+		// simple to implement
+		int evenLen = expand(s, i, i + 1);
+		int currLen = Math.max(oddLen, evenLen);
+		// update the start and end index of the longest palindrome so far
+		if (currLen > end - start) {
+		    start = i - (currLen - 1) / 2;
+		    end = i + currLen / 2;
+		}
+	    }
+	    return s.substring(start, end + 1);
+	}
+    }
+
+    public static int expand(String s, int leftCenter, int rightCenter) {
+	// the radius of the expanding
+	// leftExpPt means left expanding point, similarly for rightExpPt, they are
+	// start at the center
+	int leftExpPt = leftCenter, rightExpPt = rightCenter;
+	while (leftExpPt >= 0 && rightExpPt < s.length() && s.charAt(leftExpPt) == s.charAt(rightExpPt)) {
+	    leftExpPt--;
+	    rightExpPt++;
+	}
+	return rightExpPt - leftExpPt - 1;
     }
 
     public static String longestPalindromeMethodDP(String s) {
