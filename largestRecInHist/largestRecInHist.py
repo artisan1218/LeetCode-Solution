d6f60af7d9d8d961b113f0458dc4e4cbb11c4032
artisan1218 <zcy1218@gmail.com>
2021-09-15 22:20:25 -0700

    Update largestRecInHist.py

--- largestRecInHist/largestRecInHist.py
+++ largestRecInHist/largestRecInHist.py
@@ -1,41 +1,65 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[39]:
 
 
 from typing import List
 
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        maxArea = 0
-        stack = [] # monotonic stack
+    def largestRectangleArea1(self, heights: List[int]) -> int:
+        if len(heights)==0:
+            return 0
+        else:
+            maxArea = 0
+            stack = [] # monotonic stack
+
+            for i, h in enumerate(heights):
+                start = i
+                # when we have a bar that is lower than the height of bar on top of stack
+                # we pop the lower bar out of the stack and calculate the area of max rectangle
+                while len(stack)!=0 and stack[-1][1] > h:
+                    index, height = stack.pop()
+                    maxArea = max(maxArea, height * (i-index))
+                    start = index
+                # we only push the index, height pair when the current bar is higher than
+                # the height of bar on top of stack, this is why it's monotonic
+                stack.append((start, h)) 
+
+            # check the remaining the bar in the stack
+            # since the remaining bar are guranteed to be in increasing order, the width is simply
+            # the total length of the heights
+            for idx, height in stack:
+                maxArea = max(maxArea, height * (len(heights)-idx))
+
+            return maxArea
         
-        for i, h in enumerate(heights):
-            start = i
-            # when we have a bar that is lower than the height of bar on top of stack
-            # we pop the lower bar out of the stack and calculate the area of max rectangle
-            while len(stack)!=0 and stack[-1][1] > h:
-                index, height = stack.pop()
-                maxArea = max(maxArea, height * (i-index))
-                start = index
-            # we only push the index, height pair when the current bar is higher than
-            # the height of bar on top of stack, this is why it's monotonic
-            stack.append((start, h)) 
+    def largestRectangleArea2(self, heights: List[int]) -> int:
+        if len(heights)==0:
+            return 0
+        else:
+            maxArea = 0
+            stack = [] # monotonic stack
+            # instead of storing both index and height, we only store index
+            for i in range(len(heights)):
+                while len(stack)!=0 and heights[stack[-1]]>heights[i]:
+                    idx = stack.pop()
+                    width = i if len(stack)==0 else (i-stack[-1]-1)
+                    maxArea = max(maxArea, heights[idx] * width)
+                stack.append(i)
             
-        # check the remaining the bar in the stack
-        # since the remaining bar are guranteed to be in increasing order, the width is simply
-        # the total length of the heights
-        for idx, height in stack:
-            maxArea = max(maxArea, height * (len(heights)-idx))
-            
-        return maxArea
+            while len(stack)!=0:
+                idx = stack.pop()
+                width = len(heights) if len(stack)==0 else (len(heights)-stack[-1]-1)
+                maxArea = max(maxArea, heights[idx] * width)
+
+            return maxArea
         
         
 if __name__ == '__main__':
     solver = Solution()
-    heights = [2,1,5,6,2,3, 1]
-    print(solver.largestRectangleArea(heights))
+    heights = [4,2,0,3,2,5]
+    print(solver.largestRectangleArea2(heights))
 
 
 # In[ ]:
