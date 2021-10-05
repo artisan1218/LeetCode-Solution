d996d63972f421a584c3f9ff7a7d95aa21d80ca2
artisan1218 <zcy1218@gmail.com>
2021-10-04 21:12:42 -0700

    update

---
+++ levelOrderTraversal/levelOrderTraversal.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+# Definition for a binary tree node.
+from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def levelOrder1(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root!=None:
+            result = list()
+            level = [root]
+            result.append([node.val for node in level])
+            while len(level)!=0:
+                nextLevel = list()
+                for node in level:
+                    if node.left!=None:
+                        nextLevel.append(node.left)
+                    if node.right!=None:
+                        nextLevel.append(node.right)
+                result.append([node.val for node in nextLevel])
+                level = nextLevel
+
+            return result[:-1]
+        else:
+            return []
+    
+    def levelOrder2(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root!=None:
+            result = list()
+            queue = [root]
+            while len(queue)!=0:
+                levelLen = len(queue)
+                level = list()
+                for i in range(levelLen):
+                    node = queue.pop(0)
+                    level.append(node.val)
+                    if node.left!=None:
+                        queue.append(node.left)
+                    if node.right!=None:
+                        queue.append(node.right)
+                result.append(level)
+            return result
+        else:
+            return []
+    
+        
+
+if __name__ == '__main__':
+    solver = Solution()
+    
+    p = TreeNode(1)
+    p.left = TreeNode(2)
+    p.right = TreeNode(2)
+    p.left.left = TreeNode(3)
+    p.left.right = TreeNode(4)
+    p.right.left = TreeNode(4)
+    p.right.right = TreeNode(3)
+    
+    result = solver.levelOrder2(p)
+    print(result)
+
+
+# In[ ]:
+
+
+
+
